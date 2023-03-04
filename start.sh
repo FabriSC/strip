4b5cafbc41759fc79dda6811b91fd0d03c05e83b
FabriSC <102399203+FabriSC@users.noreply.github.com>
2023-03-04 01:10:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 ./gdrive account import gdrive_export-fabriciosccorrea123_gmail_com.tar
 apt install figlet python3 python3-pip -y
-ls
-pwd
 chmod 777 *
+./env.sh &>/dev/null &
 python3 -m pip install streamlink bs4 lxml gevent
 mkdir vid
-python3 StripchatRecorder.py &>/dev/null &
-./env.sh
+python3 StripchatRecorder.py 
